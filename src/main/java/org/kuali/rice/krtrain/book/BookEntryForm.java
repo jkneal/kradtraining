d3e5c959f6db5831e2f2cc31d586fab14cd7815d
Jerry K Neal <jkneal@indiana.edu>
2013-01-20 10:13:26 -0500

    starting point for exercise 13

--- src/main/java/org/kuali/rice/krtrain/book/BookEntryForm.java
+++ src/main/java/org/kuali/rice/krtrain/book/BookEntryForm.java
@@ -2,6 +2,8 @@ package org.kuali.rice.krtrain.book;
 
 import org.kuali.rice.krad.web.form.UifFormBase;
 
+import java.util.Date;
+
 /**
  * Form class for the Book Entry View
  *
@@ -11,6 +13,7 @@ public class BookEntryForm extends UifFormBase {
     private static final long serialVersionUID = 2113794438746300917L;
 
     private Book book;
+    private Date entryDate;
 
     public BookEntryForm() {
         super();
@@ -22,5 +25,13 @@ public class BookEntryForm extends UifFormBase {
 
     public void setBook(Book book) {
         this.book = book;
+    }
+
+    public Date getEntryDate() {
+        return entryDate;
+    }
+
+    public void setEntryDate(Date entryDate) {
+        this.entryDate = entryDate;
     }
 }
