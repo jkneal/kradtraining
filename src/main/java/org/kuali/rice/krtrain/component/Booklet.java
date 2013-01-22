281c46c311f56e5486c34738d0004b3cf2c3a833
Jerry K Neal <jkneal@indiana.edu>
2013-01-21 19:20:50 -0500

    starting point for exercise 37

---
+++ src/main/java/org/kuali/rice/krtrain/component/Booklet.java
@@ -0,0 +1,13 @@
+package org.kuali.rice.krtrain.component;
+
+import org.kuali.rice.krad.uif.widget.WidgetBase;
+
+/**
+ * Widget that renders content in a book format
+ *
+ * @author Kuali Rice Team (rice.collab@kuali.org)
+ */
+public class Booklet extends WidgetBase {
+    private static final long serialVersionUID = -7786950982028747136L;
+
+}
