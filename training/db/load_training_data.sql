27199680ef434ed70cb90c620f8f48655c3b3d46
Jerry K Neal <jkneal@indiana.edu>
2013-02-22 20:01:34 -0500

    *Master Pull* Add rice web content to training web app Update to config Zip up rice content Fixing training menu links Adding Eclipse configuration Including BookTypeViews.xml and Chapters.xml Updates to db for book data, update for book persistence, added link for book lookup Pull me 3: Added backdoor functionality, initialize author object, added sequence for book mapping, exercise 49 header, tables for book document, book type attribute def 2 min length Branch Me: added link for author lookup, fix on author id definition, added rice maintenance document, increased reloading delay Added UIF XML files, changed Rice version Update to book dictionary file, fixing imports, removing test directory from build path Update to exercise 37 link Update to rename BookType and Chapter, and removed book and book document ojb class descriptors

--- training/db/load_training_data.sql
+++ training/db/load_training_data.sql
@@ -45,4 +45,38 @@ INSERT INTO KRCR_PARM_T VALUES ('KR-SAP', 'BookEntry', 'FormatsHelpUrl', uuid(),
 
 INSERT INTO KRCR_PARM_T VALUES ('KR-SAP', 'BookEntry', 'RenderPublisherLogo', uuid(), 1, 'CONFG',
                                 'N', 'Determines whether the publisher logo is rendered on the book entry view', 'A', 'KUALI');
+                                
+TRUNCATE TABLE krtrain_book_t;
 
+INSERT INTO krtrain_book_t (id,obj_id,ver_nbr,title,typ_cd,isbn,pub_nm,pub_cd,pub_add,pub_st,fict,formats,bind_typ,lang,summary,pub_dt,price)
+  VALUES (1, '6bbbdb82-d614-49c2-8716-4234e72f9f61', 1, 'KRAD in Action!', 'IT', '34598-33', 'OReilly', '08','1356 39th New York','NY','N','P,K','Sprial','E','See the KRAD framework in Action. Full code examples',now(),'19.99');
+  
+INSERT INTO krtrain_chapter_t (book_id,num,obj_id,ver_nbr,title,part,summ,num_pages,rec,rec_page_num)
+  VALUES (1, 1, '6bbbdb82-d614-49c2-8716-4234e72f9f62', 1, 'Basics', 'Introduction','Introduction to the framework',30,'Y', 31);
+  
+INSERT INTO krtrain_chapter_t (book_id,num,obj_id,ver_nbr,title,part,summ,num_pages,rec,rec_page_num)
+  VALUES (1, 2, '6bbbdb82-d614-49c2-8716-4234e72f9f63', 1, 'Setup', 'Introduction','Project Setup',15,'Y', 16);
+  
+INSERT INTO krtrain_book_t (id,obj_id,ver_nbr,title,typ_cd,isbn,pub_nm,pub_cd,pub_add,pub_st,fict,formats,bind_typ,lang,summary,pub_dt,price)
+  VALUES (2, '6bbbdb82-d614-49c2-8716-4234e72f9f64', 1, 'Kuali Applications', 'IT', '34598-67', 'OReilly', '08','1356 39th New York','NY','N','P,K','Sprial','E','Learn all about the various Kuali Applications',now(),'39.99');
+  
+INSERT INTO krtrain_chapter_t (book_id,num,obj_id,ver_nbr,title,part,summ,num_pages,rec,rec_page_num)
+  VALUES (2, 1, '6bbbdb82-d614-49c2-8716-4234e72f9f65', 1, 'The Financial System', 'App Tour','Introduction to the Kuali financial system',13,'N', 0);
+  
+INSERT INTO krtrain_chapter_t (book_id,num,obj_id,ver_nbr,title,part,summ,num_pages,rec,rec_page_num)
+  VALUES (2, 2, '6bbbdb82-d614-49c2-8716-4234e72f9f66', 1, 'The Research System', 'App Tour','Introduction to the Kuali research system',11,'N', 0);
+
+INSERT INTO krtrain_book_t (id,obj_id,ver_nbr,title,typ_cd,isbn,pub_nm,pub_cd,pub_add,pub_st,fict,formats,bind_typ,lang,summary,pub_dt,price)
+  VALUES (3, '6bbbdb82-d614-49c2-8716-4234e72f9f67', 1, 'The Future of Software', 'SCI-FI', '34598-88', 'OReilly', '08','1356 39th New York','NY','Y','P,K','Sprial','E','A story of what software will become in the future',now(),'18.99');
+  
+INSERT INTO krtrain_chapter_t (book_id,num,obj_id,ver_nbr,title,part,summ,num_pages,rec,rec_page_num)
+  VALUES (3, 1, '6bbbdb82-d614-49c2-8716-4234e72f9f68', 1, 'New Tools', '2040','In 2040 we will see many new tools',25,'N', 0);
+  
+INSERT INTO krtrain_chapter_t (book_id,num,obj_id,ver_nbr,title,part,summ,num_pages,rec,rec_page_num)
+  VALUES (3, 2, '6bbbdb82-d614-49c2-8716-4234e72f9f69', 1, 'Software that corrects itself', '2040','In the future we will have software that finds its own bugs and applies fixes',11,'N', 0);
+
+INSERT INTO krtrain_book_id_s values (4);
+
+INSERT INTO krcr_nmspc_t values ('krtrain', uuid(), 1, 'KRAD Training', 'Y', 'RICE');
+
+UPDATE krim_perm_attr_data_t set attr_val = '*' where perm_id = '150';
