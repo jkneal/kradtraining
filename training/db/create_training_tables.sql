86b81157f0839c3fb2c39fe56a7c2c675c882e5b
Jerry K Neal <jkneal@indiana.edu>
2013-02-23 14:36:18 -0500

    *Master Pull* Add rice web content to training web app Update to config Zip up rice content Fixing training menu links Adding Eclipse configuration Including BookTypeViews.xml and Chapters.xml Updates to db for book data, update for book persistence, added link for book lookup Pull me 3: Added backdoor functionality, initialize author object, added sequence for book mapping, exercise 49 header, tables for book document, book type attribute def 2 min length Branch Me: added link for author lookup, fix on author id definition, added rice maintenance document, increased reloading delay Added UIF XML files, changed Rice version Update to book dictionary file, fixing imports, removing test directory from build path Update to exercise 37 link Update to rename BookType and Chapter, and removed book and book document ojb class descriptors Fixing link on 9, removing rice.zip

--- training/db/create_training_tables.sql
+++ training/db/create_training_tables.sql
@@ -13,7 +13,7 @@ CREATE TABLE krtrain_book_typ_t
 CREATE TABLE krtrain_author_t
 (
 	author_id BIGINT NOT NULL,
-  obj_id VARCHAR(36) NOT NULL,
+    obj_id VARCHAR(36) NOT NULL,
  	ver_nbr DECIMAL(8) NOT NULL DEFAULT 1,
 	nm VARCHAR(100),
 	address VARCHAR(200),
@@ -39,4 +39,57 @@ CREATE TABLE krtrain_pub_cd_t
 	UNIQUE krtrain_pub_cd_tc0(obj_id)
 );
 
+CREATE TABLE krtrain_book_t
+(
+	id INT NOT NULL,
+	obj_id VARCHAR(36) NOT NULL,
+	ver_nbr DECIMAL(8) NOT NULL DEFAULT 1,
+	title VARCHAR(100),
+	typ_cd VARCHAR(100),
+	isbn VARCHAR(100),
+	pub_nm VARCHAR(100),
+	pub_cd VARCHAR(5),
+	pub_add VARCHAR(100),
+	pub_st VARCHAR(2),
+	fict VARCHAR(1),
+	formats VARCHAR(200),
+	bind_typ VARCHAR(100),
+	lang VARCHAR(200),
+	summary VARCHAR(250),
+	pub_dt DATE,
+	price DECIMAL(19,2),
+	PRIMARY KEY (id),
+	UNIQUE krtrain_book_tc0(obj_id)
+);
 
+CREATE TABLE krtrain_chapter_t
+(
+	book_id INT NOT NULL,
+	num INT NOT NULL,
+	obj_id VARCHAR(36) NOT NULL,
+	ver_nbr DECIMAL(8) NOT NULL DEFAULT 1,
+	title VARCHAR(100),
+	part VARCHAR(100),
+	summ VARCHAR(200),
+	num_pages INT,
+	rec VARCHAR(1),
+	rec_page_num INT,
+	PRIMARY KEY (book_id, num),
+	UNIQUE krtrain_chapter_tc0(obj_id)
+);
+
+CREATE TABLE krtrain_book_doc_t
+(
+	fdoc_nbr VARCHAR(14) NOT NULL,
+	obj_id VARCHAR(36) NOT NULL,
+	ver_nbr DECIMAL(8) NOT NULL DEFAULT 1,
+	book_id INT NOT NULL,
+	PRIMARY KEY (fdoc_nbr),
+	UNIQUE krtrain_book_doc_tc0(obj_id)
+);
+
+CREATE TABLE krtrain_book_id_s
+(
+	id BIGINT NOT NULL AUTO_INCREMENT,
+	PRIMARY KEY (id)
+);
