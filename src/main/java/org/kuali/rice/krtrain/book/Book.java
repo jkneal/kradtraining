91caf04be67c47b00272e5c8b5e0d2768f397565
Jerry K Neal <jkneal@indiana.edu>
2013-02-24 11:00:03 -0500

    *Master Pull* Add rice web content to training web app Update to config Zip up rice content Fixing training menu links Adding Eclipse configuration Including BookTypeViews.xml and Chapters.xml Updates to db for book data, update for book persistence, added link for book lookup Pull me 3: Added backdoor functionality, initialize author object, added sequence for book mapping, exercise 49 header, tables for book document, book type attribute def 2 min length Branch Me: added link for author lookup, fix on author id definition, added rice maintenance document, increased reloading delay Added UIF XML files, changed Rice version Update to book dictionary file, fixing imports, removing test directory from build path Update to exercise 37 link Update to rename BookType and Chapter, and removed book and book document ojb class descriptors Fixing link on 9, removing rice.zip

--- src/main/java/org/kuali/rice/krtrain/book/Book.java
+++ src/main/java/org/kuali/rice/krtrain/book/Book.java
@@ -1,10 +1,8 @@
 package org.kuali.rice.krtrain.book;
 
 import org.kuali.rice.core.api.util.type.KualiDecimal;
-import org.kuali.rice.core.api.util.type.KualiInteger;
 import org.kuali.rice.krad.bo.PersistableBusinessObjectBase;
 
-import java.io.File;
 import java.util.*;
 
 /**
@@ -61,6 +59,9 @@ public class Book extends PersistableBusinessObjectBase {
 
     public Book() {
         super();
+        
+        author = new Author();
+        pubCode = new PublisherCode();
 
         isbn = "";
         edition = 1;
