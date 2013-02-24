a7f0e34811af242b42ced3fdf9d541466112edf5
Jerry K Neal <jkneal@indiana.edu>
2013-02-24 10:48:48 -0500

    *Master Pull* Add rice web content to training web app Update to config Zip up rice content Fixing training menu links Adding Eclipse configuration Including BookTypeViews.xml and Chapters.xml Updates to db for book data, update for book persistence, added link for book lookup Pull me 3: Added backdoor functionality, initialize author object, added sequence for book mapping, exercise 49 header, tables for book document, book type attribute def 2 min length Branch Me: added link for author lookup, fix on author id definition, added rice maintenance document, increased reloading delay Added UIF XML files, changed Rice version Update to book dictionary file, fixing imports, removing test directory from build path Update to exercise 37 link Update to rename BookType and Chapter, and removed book and book document ojb class descriptors Fixing link on 9, removing rice.zip

--- src/main/java/org/kuali/rice/krtrain/KrtrainMenuController.java
+++ src/main/java/org/kuali/rice/krtrain/KrtrainMenuController.java
@@ -1,11 +1,17 @@
 package org.kuali.rice.krtrain;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.kuali.rice.krad.UserSession;
+import org.kuali.rice.krad.util.GlobalVariables;
 import org.kuali.rice.krad.web.controller.UifControllerBase;
 import org.kuali.rice.krad.web.form.UifFormBase;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import javax.servlet.http.HttpServletRequest;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * Controller for the training menu view
@@ -18,5 +24,15 @@ public class KrtrainMenuController extends UifControllerBase {
 
     protected UifFormBase createInitialForm(HttpServletRequest httpServletRequest) {
         return new KrtrainMenuForm();
+    }
+    
+    @RequestMapping(params = "methodToCall=login")
+    public ModelAndView login(@ModelAttribute("KualiForm") KrtrainMenuForm form, BindingResult result,
+            HttpServletRequest request, HttpServletResponse response) {
+    	
+    	UserSession userSession = GlobalVariables.getUserSession();
+    	userSession.setBackdoorUser(request.getParameter("backdoorId"));
+        
+    	return getUIFModelAndView(form);
     }
 }
