aa9a631ab3d81093a38dbd1861029e592a664127
jkneal <jkneal@indiana.edu>
2013-09-27 11:52:42 -0700

    Service Impl

---
+++ src/main/java/org/kuali/rice/krtrain/service/BookService.java
@@ -0,0 +1,8 @@
+package org.kuali.rice.krtrain.service;
+
+import org.kuali.rice.krtrain.book.Book;
+
+public interface BookService {
+
+	void processBook(Book book);
+}
---
+++ src/main/java/org/kuali/rice/krtrain/service/impl/BookServiceImpl.java
@@ -0,0 +1,14 @@
+package org.kuali.rice.krtrain.service.impl;
+
+import org.kuali.rice.krtrain.book.Book;
+import org.kuali.rice.krtrain.service.BookService;
+
+public class BookServiceImpl implements BookService {
+
+	@Override
+	public void processBook(Book book) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
